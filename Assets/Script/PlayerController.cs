fab1500dc9636be61dd54dc54e729ae3f2b15709
KIC-GAME-15\student <kof2464733@stu.o-hara.ac.jp>
2025-09-03 11:58:10 +0900

    Reapply "Merge branch 'master' into player"
    This reverts commit 1a1e72392ea61bec032a1009c29e2ac5e7b0745a.

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+<<<<<<< HEAD
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SimplePlayerController : MonoBehaviour
+{
+    private Vector2 moveInput = Vector2.zero;
+    private bool jumpInput = false;
+
+    private Rigidbody rb;
+
+    public float speed = 5f;
+    public float jumpForce = 5f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jumpInput = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // 移動方向計算
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+
+        // 移動処理
+        Vector3 velocity = move.normalized * speed;
+        velocity.y = rb.velocity.y;  // 垂直速度は維持
+
+        rb.velocity = velocity;
+
+        // ジャンプ処理（地面判定なし）
+        if (jumpInput)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            jumpInput = false;
+        }
+    }
+=======
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0f, moveZ) * moveSpeed;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = move.x;
+        velocity.z = move.z;
+        rb.velocity = velocity;
+    }
+
+    void Jump()
+    {
+        // すべてのコライダーに対して地面チェックする
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // 地面検出用Rayを確認したいときはここをON
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 1.1f);
+    }
+>>>>>>> master
+}
