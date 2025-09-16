ed24bc428edc1ae86f6c45140b80007cede43c4c
pakupaku <kof2464706@o-hara.stu.ac.jp>
2025-09-16 09:53:08 +0900

    クリアを全体共有の変数にした

---
+++ Assets/Script/ShareVariable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShareVariable : MonoBehaviour
+{
+    public static class Share
+    {
+        public static bool clear = false;
+    }
+}
