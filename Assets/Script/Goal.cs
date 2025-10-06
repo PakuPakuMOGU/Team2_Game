0374aba9fdd7c8e16369ee5b3f59b44b978d17f0
pakupaku <kof2464706@o-hara.stu.ac.jp>
2025-10-06 11:43:36 +0900

    ゴールしたらパネルがフェードインするようになった

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -2,44 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     [Header("クリアポイント設定")]
     public GameObject goalPoint;
 
-    [System.Serializable]
-    public class FadeClass
-    {
-        public bool yes = true;
-        public FadeInOut fadeScript;
-    }
-    [Header("フェードアウトする？")]
-    [SerializeField] private FadeClass fade;
-    private bool fadeStarted = false;
+    [Header("クリアディスプレイ表示")]
+    public GameObject clearDisplay;
 
     void Start()
     {
-        
+        clearDisplay.SetActive(false);
     }
 
     void Update()
     {
-        if (ShareVariable.Share.clear && !fadeStarted)
-        {
-            fadeStarted = true;
-
-            if (!fade.yes)
-            {
-                ReturnMainScene();           // フェードしない場合すぐに移行.
-            }
-            else
-            {
-                fade.fadeScript.OnFadeComplete = ReturnMainScene;
-                fade.fadeScript.StartFade();
-            }
-        }
+       
     }
 
     void OnTriggerEnter(Collider collider)
@@ -47,12 +26,8 @@ public class Goal : MonoBehaviour
         if (collider.CompareTag("Player"))
         {
             Debug.Log("Clear!!!");
+            clearDisplay.SetActive(true);
             ShareVariable.Share.clear = true;   // 全体共有の変数を変更.
         }
-    }
-
-    void ReturnMainScene()
-    { 
-        SceneManager.LoadScene("MainScene");
     }
 }
---
+++ Assets/Script/Player/GoTitle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoTitle : MonoBehaviour
+{
+    [System.Serializable]
+    public class FadeClass
+    {
+        public bool yes = true;
+        public FadeInOut fadeScript;
+    }
+    [Header("フェードアウトする？")]
+    [SerializeField] private FadeClass fade;
+
+    void Update()
+    {
+        
+    }
+
+    public void FadeStart()
+    {
+        if (ShareVariable.Share.clear)
+        { 
+            if (!fade.yes)
+            {
+                ReturnMainScene();           // フェードしない場合すぐに移行.
+            }
+            else
+            {
+                fade.fadeScript.OnFadeComplete = ReturnMainScene;
+                fade.fadeScript.StartFade();
+            }
+        }
+    }
+
+    void ReturnMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
