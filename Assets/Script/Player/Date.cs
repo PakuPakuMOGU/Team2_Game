2f4aa2793e9d8d3165cec788364d6f23dbe9a584
pakupaku <kof2464706@o-hara.stu.ac.jp>
2025-10-20 12:06:35 +0900

    経過時間の解像度を上げた。あと経過時間をセーブデータの持越しに入れた

--- Assets/Script/Player/Date.cs
+++ Assets/Script/Player/Date.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerData
 {
     public Vector3 playerPosition;
+    public float time;
 }
 
 public class Date: MonoBehaviour
 {
+    public ClearTime timeSc;
+
     public void Save()
     {
         PlayerData data = new PlayerData();
         data.playerPosition = this.transform.position;
+        data.time = timeSc.ReturnNowTime();
 
         string json = JsonUtility.ToJson(data, true); // å©Ç‚Ç∑Ç¢å`éÆÇ≈
         File.WriteAllText(Application.persistentDataPath + "/save.json", json);
@@ -27,6 +31,7 @@ public class Date: MonoBehaviour
             string json = File.ReadAllText(path);
             PlayerData data = JsonUtility.FromJson<PlayerData>(json);
             this.transform.position= data.playerPosition;
+            timeSc.GiveNowTime(data.time);
 
             Debug.Log("à íuÅF" + data.playerPosition);
         }
